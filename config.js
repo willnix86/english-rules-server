d34a0a8138feae933d6628eb99528ebda55b3e29
Will Nixon <will_nixon@me.com>
2018-12-09 15:21:24 -0800

    added CORS

---
+++ config.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+'use strict';
+
 const express = require('express');
+const cors = require('cors');
+const {PORT, CLIENT_ORIGIN} = require('./config');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
